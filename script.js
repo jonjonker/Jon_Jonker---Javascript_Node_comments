29ec791d0c7a6308532de95f2da6dc8cc0a0bfba
Jón Jonker <66784287+jonjonker@users.noreply.github.com>
2020-12-03 14:24:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Hallo Winc Academy Studenten');
+
+
+// dit is een grote som
+
+// let multiplier = 1230941 * 1823794;
+// console.log(multiplier);
+
+// let divider = 637263 / 54;
+// console.log(divider);
